a86775643a7e7a85beef22203db213555363788b
Aniket Lahane <lahaneaniket179@gmail.com>
2026-03-29 23:47:33 +0530

    data cleaning process start

---
+++ ml_model.py
@@ -0,0 +1,28 @@
+import pandas as pd
+
+
+
+
+data_set =pd.read_csv(r"C:\Users\anike\OneDrive\Desktop\final project\student_dataset_10000.csv")
+# print(data_set.head)
+# print(data_set.columns)
+print(data_set.dtypes)
+column_to_remove=['student_id','name', 'income_bracket','hindi_comfort','regional_lang_comfort', 'career_interest_1','career_interest_2' ];
+dataset1 =data_set.drop(columns=column_to_remove);
+# print(dataset1)
+# print(dataset1.columns)
+# print(dataset1.shape);
+# print(dataset1.dtypes);
+print("which column contain  null value")
+dataset1.isnull().sum()
+dataset1['internet_type']=dataset1['internet_type'].fillna("Not_internate");
+print(dataset1.isnull().sum())
+dataset1['english_comfort']=dataset1['english_comfort'].fillna(dataset1['english_comfort'].mode()[0])
+print(dataset1.isnull().sum());
+print("number of  dulpicated count: ")
+print(dataset1.duplicated().sum())
+print("which row are duplicated: ")
+print(dataset1[dataset1.duplicated()])
+dataset1=dataset1.drop_duplicates();
+print("show again  number  of duplicated value")
+print(dataset1.duplicated().sum())
